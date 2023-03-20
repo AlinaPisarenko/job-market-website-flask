d8d10ea0890e63ea30aa63f31c3c2010a8014f09
Alina <pisarenko.alina@gmail.com>
2023-03-20 00:19:09 -0400

    connected to db

---
+++ database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine, text
+from config import db_connection_string
+
+
+engine = create_engine(
+  db_connection_string, 
+  connect_args={
+    "ssl": {
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  })
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      obj = {
+        'id': row[0],
+        'title': row[1],
+        'location': row[2],
+        'salary': row[3],
+        'currency': row[4],
+        'description': row[5],
+        'requirements': row[6]
+      }
+      jobs.append((obj))
+    return jobs
