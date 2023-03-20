efd465782b7b5ea4e24b82009e6e5f3b45203ff0
Alina Pisarenko <pisarenko.alina@gmail.com>
2023-03-20 15:44:15 -0400

    added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -14,6 +14,14 @@ def load_home():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route('/job/<id>')
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not found 😔"
+  return render_template('jobpage.html', job = job)
+
 
 if __name__ == '__main__':
   app.run(port=5555, debug=True)
--- database.py
+++ database.py
@@ -1,10 +1,10 @@
 from sqlalchemy import create_engine, text
 import os
 
-q = os.environ.get('DB_CONN_STR')
-print("var", q)
+conn_str = os.environ.get('DB_CONN_STR')
+
 engine = create_engine(
-  q, 
+  conn_str, 
   connect_args={
     "ssl": {
       "ssl_ca": "/etc/ssl/cert.pem"
@@ -17,7 +17,7 @@ def load_jobs_from_db():
     result = conn.execute(text("select * from jobs"))
     jobs = []
     for row in result.all():
-      obj = {
+      dict = {
         'id': row[0],
         'title': row[1],
         'location': row[2],
@@ -26,5 +26,29 @@ def load_jobs_from_db():
         'description': row[5],
         'requirements': row[6]
       }
-      jobs.append((obj))
+      jobs.append(dict)
     return jobs
+
+
+def load_job_from_db(id):
+  print("HERE IS AN ID", id)
+  with engine.connect() as conn:
+    stmt = text("SELECT * FROM jobs WHERE id = :x").bindparams(x=id)
+    result = conn.execute(stmt).fetchall()
+
+    if len(result) == 0:
+      return None
+    else:
+      res = result[0]
+      dict = {
+        'id': res[0],
+        'title': res[1],
+        'location': res[2],
+        'salary': res[3],
+        'currency': res[4],
+        'description': res[5],
+        'requirements': res[6]
+      }
+      return dict
+
+
